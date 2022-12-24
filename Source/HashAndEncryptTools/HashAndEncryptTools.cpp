22fd084b9237ca89a34de7af5524be0ce82699e7
JasonHoku <jlevien808@gmail.com>
2022-12-24 02:16:49 -1000

    git init for 1.2

---
+++ Source/HashAndEncryptTools/HashAndEncryptTools.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "HashAndEncryptTools.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, HashAndEncryptTools, "HashAndEncryptTools" );
